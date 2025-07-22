c570dec92780ffa7889b10b6895c3e43dcb2b473
arikusumastuti <arikusumastuti@gmail.com>
2025-07-22 09:31:56 +0700

    add: text "Click Here" in Cover

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -12,8 +12,9 @@ export default function Cover () {
                 <h1 className="text-xl md:text-3xl xl:text-5xl font-extrabold">Metabolic Networks Features</h1>
                 <p className="md:text-xl xl:text-2xl">By Ari Kusumastuti</p>
             </div>
-            <div className="absolute w-full left-0 bottom-0 flex justify-center py-12">
-                <HiChevronDoubleDown className="text-5xl animate-bounce text-yellow-300" />
+            <div className="absolute w-full left-0 bottom-0 flex flex-col items-center justify-center py-12">
+                <span className="font-bold mb-2">Click Here</span>
+                <HiChevronDoubleDown className="text-5xl text-yellow-300 animate-bounce" />
             </div>
         </div>
     )
